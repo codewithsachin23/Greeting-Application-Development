86a105c0628f1b8d24d4c8420f60aab8ad152d31
sachin shukla <sachinshukla232003@gmail.com>
2025-02-22 22:56:31 +0530

    [Sachin Shukla]Add. UC-3 Say greeting Post request and with condition

--- src/main/java/com/greetingappdevelepment/controllers/GreetingController.java
+++ src/main/java/com/greetingappdevelepment/controllers/GreetingController.java
@@ -1,6 +1,27 @@
 package com.greetingappdevelepment.controllers;
 
+import com.greetingappdevelepment.model.User;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/greeting")
 public class GreetingController {
 
+@PostMapping
+public String greeting(@RequestBody User user) {
+    if (user.getFirstName() != null && !user.getFirstName().isEmpty() &&
+            user.getLastName() != null && !user.getLastName().isEmpty()) {
+        return "Hello, " + user.getFirstName() + " " + user.getLastName() + "!";
+    } else if (user.getFirstName() != null && !user.getFirstName().isEmpty()) {
+        return "Hello, " + user.getFirstName() + "!";
+    } else if (user.getLastName() != null && !user.getLastName().isEmpty()) {
+        return "Hello, " + user.getLastName() + "!";
+    } else {
+        return "Hello World!";
+    }
+}
 
 }
