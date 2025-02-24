331a2abbafcdae1baa8885e60057113f96984fc0
sachin shukla <sachinshukla232003@gmail.com>
2025-02-24 10:47:02 +0530

    [Sachin Shukla]Add. UC-4 add Greeting in Repository

--- src/main/java/com/greetingappdevelepment/controllers/GreetingController.java
+++ src/main/java/com/greetingappdevelepment/controllers/GreetingController.java
@@ -1,6 +1,25 @@
 package com.greetingappdevelepment.controllers;
 
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/greeting")
 public class GreetingController {
+
+    private HashMap<Integer,String> map=new HashMap<>();
+    private int key;
+    @GetMapping
+    public Map<Integer, String> getMessage(){
+        return map;
+    }
+    @PostMapping
+    public String createGreeting(@RequestBody String message){
+        map.put(key,message);
+        return "Greeting message was added"+key++;
+    }
 
 
 }
